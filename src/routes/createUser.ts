f4d15e90063f413b3dc873819bd2a9a6ce6c7cd3
saarangdev <saarang.tiwari@hotmail.com>
2019-04-11 10:49:49 +0530

    Checking if the user already exists or not before adding him

--- src/routes/createUser.ts
+++ src/routes/createUser.ts
@@ -3,14 +3,26 @@ import {
   Response
 } from 'express';
 import UserClass from '../models/userClass';
+import UserIntf from '../interfaces/userIntf';
 
 export default class CreateUser {
   public routes(app): void {
     app.route('/create_user')
       .post((req: Request, res: Response) => {
         const body = req.body;
+        this.checkExistenceAndAddUser({ name: body.userName, password: body.password }, res);
+      });
+  }
 
-        UserClass.addUser({ name: body.userName, password: body.password }).then((result) => {
+  private checkExistenceAndAddUser(userObject: UserIntf, res: Response) {
+    UserClass.isAuthorized(userObject).then((result: []) => {
+      if (result.length) {
+        res.status(500);
+        res.send('user already exists');
+        res.end();
+      }
+      else {
+        UserClass.addUser(userObject).then((result) => {
           res.send(result);
           res.status(200);
           res.end();
@@ -19,6 +31,11 @@ export default class CreateUser {
           res.status(500);
           res.end();
         });
-      })
+      }
+      
+    }).catch((reason) => {
+      res.send('Error reason: ' + reason);
+      res.end();
+    });
   }
 }
