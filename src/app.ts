6205e3ac2e94ed73810023d76ed3faba7ca088a9
saarangdev <saarang.tiwari@hotmail.com>
2019-04-17 17:57:45 +0530

    ADded CORS

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,10 @@ import RouteGenerator from "./routes/routeGenerator";
 const app = express();
 const httpServer = http.createServer(app);
 const port = 3000;
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+});
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
